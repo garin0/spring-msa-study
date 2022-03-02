ec80b37e5ba89ebd336a3ea4f21980e08def1eeb
김가린 <garin0112@naver.com>
2022-03-02 22:46:01 +0900

    spring cloud gateway filter-1

--- first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
@@ -1,4 +1,4 @@
-package example.firstservice;
+package com.example.firstservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
